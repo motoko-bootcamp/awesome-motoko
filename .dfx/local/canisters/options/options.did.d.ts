66f4d62ed87848f5adad4e5f519983e845cebaae
Nambi <harrisonlvl2@gmail.com>
2022-04-14 20:23:12 +0530

    Delete .dfx directory

--- .dfx/local/canisters/options/options.did.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-export interface _SERVICE {
-  'multipleOfThree' : (arg_0: bigint) => Promise<[] | [bigint]>,
-  'optionToNat' : (arg_0: [] | [bigint]) => Promise<bigint>,
-}
